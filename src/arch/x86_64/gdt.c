5fadc0ff1458f08cb3345dd849b77ca143da6063
CaydendW <cayden@4dewits.com>
2021-06-30 15:51:48 +0200

    Added user segments to GDT

--- src/arch/x86_64/gdt.c
+++ src/arch/x86_64/gdt.c
@@ -35,18 +35,35 @@ int init_gdt() {
                          .limit = 0x0,
                          .access = 0x0,
                          .granularity = 0x0}; // NULL descriptor
+  
   gdt[1] = (gdt_entry_t){.low = 0x0,
                          .mid = 0x0,
                          .high = 0x0,
                          .limit = 0x0,
                          .access = 0x9a,
-                         .granularity = 0x20}; // Code descriptor
+                         .granularity = 0x20}; // Kernel code descriptor
+
   gdt[2] = (gdt_entry_t){.low = 0x0,
                          .mid = 0x0,
                          .high = 0x0,
                          .limit = 0x0,
                          .access = 0x92,
-                         .granularity = 0x0}; // Data descriptor
+                         .granularity = 0x0}; // Kernel data descriptor
+  
+  gdt[3] = (gdt_entry_t){.low = 0x0,
+                         .mid = 0x0,
+                         .high = 0x0,
+                         .limit = 0x0,
+                         .access = 0xfa,
+                         .granularity = 0x20}; // User data descriptor
+  
+  gdt[4] = (gdt_entry_t){.low = 0x0,
+                         .mid = 0x0,
+                         .high = 0x0,
+                         .limit = 0x0,
+                         .access = 0xf2,
+                         .granularity = 0x0}; // User data descriptor
+
 
   load_gdt(gdt_ptr);
 
