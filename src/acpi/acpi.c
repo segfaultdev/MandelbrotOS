9bc3a3c55b1d7cd0a68e5263515838d677d64611
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-07-02 08:03:54 +0530

    ACPI 2.0 support

--- src/acpi/acpi.c
+++ src/acpi/acpi.c
@@ -9,8 +9,6 @@ static rsdp_t *rsdp;
 static rsdt_t *rsdt;
 static xsdt_t *xsdt;
 
-// TODO: ACPI 2.0+ support
-
 bool do_acpi_checksum(sdt_t *th) {
   uint8_t sum = 0;
 
@@ -21,11 +19,22 @@ bool do_acpi_checksum(sdt_t *th) {
 }
 
 void *get_table(char *signature, int index) {
-  int entries = (rsdt->h.length - sizeof(rsdt->h)) / 4;
+  int entries;
+  sdt_t *h;
+
+  if (!rsdp->revision)
+    entries = (rsdt->h.length - sizeof(rsdt->h)) / 4;
+  else
+    entries = (xsdt->h.length - sizeof(xsdt->h)) / 8;
 
   int i = 0;
   for (int t = 0; t < entries; t++) {
-    sdt_t *h = (sdt_t *)(uint64_t)rsdt->sptr[t];
+    
+    if (!rsdp->revision)
+      h = (sdt_t *)(uint64_t)rsdt->sptr[t];
+    else
+      h = (sdt_t *)xsdt->sptr[i];
+    
     if (!strncmp(signature, h->signature, 4)) {
       if (do_acpi_checksum(h) && i == index)
 	return (void *)h;
@@ -42,11 +51,10 @@ void *get_table(char *signature, int index) {
 int init_acpi(struct stivale2_struct_tag_rsdp *rsdp_info) {
   rsdp = (rsdp_t *)rsdp_info->rsdp;
   
-  if (!rsdp->revision) {
+  if (!rsdp->revision)
     rsdt = (rsdt_t *)(uint64_t)rsdp->rsdt_address;
-  } else {
+  else
     xsdt = (xsdt_t *)rsdp->xsdt_address;
-  } 
 
   return 0;
 }
