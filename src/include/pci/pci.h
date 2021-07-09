c56b5dd58245d778de8198ee85d812f50497582a
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-07-09 10:25:56 +0530

    PCI enumeration with ACPI/MCFG

--- src/include/pci/pci.h
+++ src/include/pci/pci.h
@@ -1,24 +1,28 @@
 #ifndef __PCI_H__
 #define __PCI_H__
-
+#include <acpi/tables.h>
 #include <stdint.h>
 
-typedef struct pci_device {
-  uint8_t bus;
-  uint8_t device;
-  uint8_t function;
-  uint8_t class;
-  uint8_t subclass;
-  uint16_t device_id;
+typedef struct pci_edev {
   uint16_t vendor_id;
-} pci_t;
+  uint16_t device_id;
+  uint16_t command;
+  uint16_t status;
+  uint8_t revision_id;
+  uint8_t prog_if;
+  uint8_t subclass;
+  uint8_t class;
+  uint8_t cache_lsize;
+  uint8_t latency_timer;
+  uint8_t header_type;
+  uint8_t bist;
+} pci_device_t;
 
 extern const char *pci_device_classes[];
 
 const char *pci_get_subclass_name(uint8_t class, uint8_t subclass);
 const char *pci_get_vendor_name(uint16_t vendor_id);
 const char *pci_get_device_name(uint16_t vendor_id, uint16_t device_id);
-void pci_legacy_enumerate();
 int pci_enumerate();
 
 #endif
--- src/pci/pci.c
+++
@@ -1,6 +0,0 @@
-#include <pci/pci.h>
-
-int pci_enumerate() {
-  pci_legacy_enumerate();
-  return 0;
-}
